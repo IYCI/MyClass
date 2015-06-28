e61196dd7f2c6e19536ed5f84b0ff6080c601b19
19506jk <pengdanny23@gmail.com>
2015-06-28 02:26:26 -0400

    Add license for helper files

--- app/src/main/java/com/example/jason/myclass/Reminder/helpers/SimpleItemTouchHelperCallback.java
+++ app/src/main/java/com/example/jason/myclass/Reminder/helpers/SimpleItemTouchHelperCallback.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 Paul Burke
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.jason.myclass.Reminder.helpers;
 
 import android.support.design.widget.Snackbar;
@@ -5,9 +21,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 
-/**
- * Created by Danny on 2015/6/28.
- */
+
 public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
 
     private final ItemTouchHelperAdapter mAdapter;
